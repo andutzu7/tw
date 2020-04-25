87d99462acacb1fb1994c1ee8734bc99fd7027f7
Teofil-Constantin Ciobanu <teofil.ciobanu@bytex.ro>
2020-04-25 18:07:59 +0300

    API to get all rows from any table

--- api/hello-world.js
+++ api/hello-world.js
@@ -1,36 +1,49 @@
 const http = require('http');
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: 'eu-cdbr-west-03.cleardb.net',
-  user: 'b4ce0916cecd87',
-  password: '0aa128f5',
-  database: 'heroku_79d4353e46b22ee'
-});
+const url = require('url');
+const database = require('./database');
 
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('connected to db');
-});
 
-connection.query('SELECT * FROM varste where id_judet=10', (err,rows) => {
-    if(err) throw err;
-  
-    console.log('Data received from Db:');
-    rows.forEach( (row) => {
-        console.log(`luna: ${row.luna}, sub 25 ani: ${row.sub25}, peste 55: ${row.peste55}`);
-      });
-  });
+db = new database.Database('eu-cdbr-west-03.cleardb.net', 'b4ce0916cecd87', '0aa128f5', 'heroku_79d4353e46b22ee');
+
+data = {}
+
+connection = db.getConnection();
+
+all_tables = ['varste', 'medii', 'educatie', 'rata', 'judete']
+
+for(let table_name of all_tables){
+    connection.query(`SELECT * FROM ${table_name}`, (err, rows) => {
+        if (err) throw err;
+        data[table_name] = JSON.parse(JSON.stringify(rows));
+    });
+}
+connection.end()
+
+
+
+
 
 
 const hostname = '127.0.0.1';
 const port = 3000;
-
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+    valid_url = false;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    for(let table_name of all_tables){
+        if(req.url.substring(1) == table_name){
+            valid_url = true;
+            console.log(req.url);
+
+            res.end(JSON.stringify(data[table_name]))
+            break;
+        }
+    }
+    if(!valid_url){
+        console.log("INVALID URL: " + req.url);
+        res.end('INVALID URL')
+    }
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,10 @@
-function test() {
-    console.log("hello world");
-}
 
-test()
+fetch('http://127.0.0.1:3000/rata')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    for(let row of data){
+        console.log(row); //do stuff with row
+    }
+  });
