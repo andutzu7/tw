328b4e61d6f3f0125c45ab4740feaac404c0f6ba
andutzu7 <rockkidd57@gmail.com>
2020-05-09 02:53:38 +0300

    added tooltip to the map

--- Public/SCRIPTS/svgMapChart.js
+++ Public/SCRIPTS/svgMapChart.js
@@ -1,4 +1,4 @@
-function set_county_color(county_id, color){
+function set_county_color(county_id, color) {
     console.assert(county_id > 0 && county_id <= 42, `invalid county_id: ${county_id}`)
     var element_id = `JUDET_SVG_${county_id}`
 
@@ -20,18 +20,58 @@ function map_value_to_color(value){
 }
 
 
-function myfunc(API_URL, TABLE_NAME){
+function myfunc(API_URL, TABLE_NAME) {
     fetch(`${API_URL}/${TABLE_NAME}`)
         .then((response) => {
             return response.json();
         })
         .then((data) => {
-            for(let row of data){
-                if(row.luna == 1){ //hardcoded
-                    set_county_color(row.id_judet, map_value_to_color(row.total))
+            let array = [];
+
+            for (let row of data) {
+
+                if (row.luna == 1) { //hardcoded
+                    set_county_color(row.id_judet, map_value_to_color(row.total));
+                    array.push(row.total);//also to be changed
                 }
             }
+            let tooltip = document.getElementById('tooltip');
+            let triggers = document.getElementsByClassName('tooltip-trigger');
+            for (let i = 0; i < triggers.length; i++) {
+                triggers[i].addEventListener('mousemove', function (event) {
+                    let CTM = svg.getScreenCTM();
+                    let x = (event.clientX - CTM.e + 6) / CTM.a;
+                    let y = (event.clientY - CTM.f + 20) / CTM.d;
+                    tooltip.setAttributeNS(null, "transform", "translate(" + x + " " + y + ")");
+                    tooltip.setAttributeNS(null, "visibility", "visible");
+                    let tooltipText = tooltip.getElementsByTagName('text')[0];
+                    tooltipText.firstChild.data = "Numarul total de someri: " + array[i];
+                    let tooltipRects = tooltip.getElementsByTagName('rect');
+                    let length = tooltipText.getComputedTextLength();
+                    for (let i = 0; i < tooltipRects.length; i++) {
+                        tooltipRects[i].setAttributeNS(null, "width", length + 8);
+                    }
+
+
+
+                });
+                triggers[i].addEventListener('mouseout', function (event) {
+
+                    tooltip.setAttributeNS(null, "visibility", "hidden");
+
+                });
+            }
+
+
+            let svg = document.getElementById('romanian_map');
+
+
+        })
+        .then((fetched_data) => {
+
+
         });
 }
+
 url = 'https://arcane-sierra-19327.herokuapp.com'
 myfunc(url, 'rata')
