27dea6a243f4eb94f728b9a3df95285a0c5925f7
Teofil-Constantin Ciobanu <teofil.ciobanu@bytex.ro>
2020-05-13 20:56:14 +0300

    css for download button

--- Public/SCRIPTS/showDiv.js
+++ Public/SCRIPTS/showDiv.js
@@ -1,12 +1,13 @@
-function showDiv(elem) {
+function showDiv(value) {
     for(var i=0; i < divsO.length; i++) {
         divsO[i].style.display = 'none';
     }
     //unhide the selected div
-    document.getElementById('hidden_div'+elem.value).style.display = 'block';
+    document.getElementById('hidden_div'+value).style.display = 'block';
 }
  
 window.onload=function() {
     //get the divs to show/hide
     divsO = document.getElementById("form-criterii").getElementsByTagName('div');
+    this.showDiv(3);
 }
