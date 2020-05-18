33466630dcc2ec60be7d70dbb948f5baa567b5f0
andutzu7 <rockkidd57@gmail.com>
2020-05-18 13:44:08 +0300

    added table head

--- tableSortHtml/Sort.js
+++ tableSortHtml/Sort.js
@@ -1,66 +1,45 @@
 function generate_table() {
-    // get the reference for the body
+    generateTable(all_tables);
+}
+
+function generateTable(allTables) {
+
     var body = document.getElementsByTagName("body")[0];
-  
-    // creates a <table> element and a <tbody> element
     var tbl = document.createElement("table");
     var tblBody = document.createElement("tbody");
-  
-    // creating all cells
-    for (var i = 0; i < 6; i++) {
-      // creates a table row
-      var row = document.createElement("tr");
-  
-      for (var j = 0; j < 2; j++) {
-        // Create a <td> element and a text node, make the text
-        // node the contents of the <td>, and put the <td> at
-        // the end of the table row
-        var cell = document.createElement("td");
-        var cellText = document.createTextNode("cell in row "+i+", column "+j);
-        cell.appendChild(cellText);
-        row.appendChild(cell);
-      }
-  
-      // add the row to the end of the table body
-      tblBody.appendChild(row);
-    }
-  
-    // put the <tbody> in the <table>
-    tbl.appendChild(tblBody);
-    // appends <table> into <body>
-    body.appendChild(tbl);
-    // sets the border attribute of tbl to 2;
-    tbl.setAttribute("border", "2");
-    
-   generateCSV(all_tables);
-  }
 
-
-
-  function generateCSV(data) {
-    let csvString = "";//trebuie puse categoriile in csv
-    var urban_femei = "urban_femei";
-    for (const id in data) {
-        for (const month in data[id])
-            for (let id_judet in data[id][month]) {
-                for (let key in data[id][month][id_judet]) {
-                    if (csvString === "") {//facem headeru
-                        for (let aux in data[id][month][id_judet]) {
-                            csvString += aux + ","
-                            for(let i in data[id][month][id_judet][aux]){
-                                console.log(data[id][month][id_judet][aux] + "-" + data[id][month][id_judet][aux][i])
+    let tableHeader = "";
+    let tableRow = "";
+    for (let category in allTables) {
+        if (category === "rata") {
+            for (let year in allTables[category]) {
+                for (let month in allTables[category][year]) {
+                    for (let id_judet in allTables[category][year][month]) {
+                        let row = document.createElement("tr");
+                        for (let key in allTables[category][year][month][id_judet]) {
+                            if (key !== "luna" && key !== "an" && key !== "id_judet") {
+                                if (tableHeader === "") {
+                                    for (const headerName in allTables[category][year][month][id_judet]) {
+                                        if (headerName !== "luna" && headerName !== "an" && headerName !== "id_judet") {
+                                            let cell = document.createElement("td");
+                                            let cellText = document.createTextNode(headerName);
+                                            tableHeader += headerName;
+                                            cell.appendChild(cellText);
+                                            row.appendChild(cell);
+                                        }
+                                    }
+                                }
                             }
                         }
-
-                        csvString = csvString.substring(0, csvString.length - 1);
-                        csvString += '\n';
+                        tblBody.appendChild(row);
                     }
-                    csvString += data[id][month][id_judet][key] + ',';
                 }
-                csvString = csvString.substring(0, csvString.length - 1);
-                csvString += '\n';
-
             }
+        }
     }
-    return csvString;
+
+    tbl.appendChild(tblBody);
+    body.appendChild(tbl);
+    tbl.setAttribute("border", "2");
+
 }
