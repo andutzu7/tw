a1533fd56e1b57c5e9c2af95003ad2d39af1a693
Teofil-Constantin Ciobanu <teofil.ciobanu@bytex.ro>
2020-05-21 08:21:38 +0300

    table data is limited per one month

--- Public/SCRIPTS/table.js
+++ Public/SCRIPTS/table.js
@@ -3,6 +3,8 @@ function generate_table() {
 }
 
 function generateTable(allTables) {
+    month = 3
+    year = 2020
 
     var table = document.getElementById("Table");
     var tbl = document.createElement("table");
@@ -12,36 +14,32 @@ function generateTable(allTables) {
     let tableHeader = "";
     for (let category in allTables) {
         if (category === "rata") {
-            for (let year in allTables[category]) {
-                for (let month in allTables[category][year]) {
-                    for (let id_judet in allTables[category][year][month]) {
-                        let row = document.createElement("tr");
-                        for (let key in allTables[category][year][month][id_judet]) {
-                            if (key !== "luna" && key !== "an") {
-                                if (tableHeader === "") {
-                                    let the_header = document.createElement("thead");
-                                    let headrow = document.createElement("tr");
-                                    for (const headerName in allTables[category][year][month][id_judet]) {
-                                        if (headerName !== "luna" && headerName !== "an") {
-                                            let cell = document.createElement("th");
-                                            let cellText = document.createTextNode(headerName);
-                                            tableHeader += headerName;
-                                            cell.appendChild(cellText);
-                                            headrow.appendChild(cell);
-                                            the_header.appendChild(headrow);
-                                        }
-                                    }
-                                    tbl.appendChild(the_header);
+            for (let id_judet in allTables[category][year][month]) {
+                let row = document.createElement("tr");
+                for (let key in allTables[category][year][month][id_judet]) {
+                    if (key !== "luna" && key !== "an") {
+                        if (tableHeader === "") {
+                            let the_header = document.createElement("thead");
+                            let headrow = document.createElement("tr");
+                            for (const headerName in allTables[category][year][month][id_judet]) {
+                                if (headerName !== "luna" && headerName !== "an") {
+                                    let cell = document.createElement("th");
+                                    let cellText = document.createTextNode(headerName);
+                                    tableHeader += headerName;
+                                    cell.appendChild(cellText);
+                                    headrow.appendChild(cell);
+                                    the_header.appendChild(headrow);
                                 }
-                                let cell = document.createElement("td");
-                                let cellText = document.createTextNode(allTables[category][year][month][id_judet][key]);
-                                cell.appendChild(cellText);
-                                row.appendChild(cell);
                             }
+                            tbl.appendChild(the_header);
                         }
-                        tblBody.appendChild(row);
+                        let cell = document.createElement("td");
+                        let cellText = document.createTextNode(allTables[category][year][month][id_judet][key]);
+                        cell.appendChild(cellText);
+                        row.appendChild(cell);
                     }
                 }
+                tblBody.appendChild(row);
             }
         }
     }
@@ -67,7 +65,6 @@ for (let c = 0; c < th.length; c++) {
 
 
 function item(c) {
-
     return function () {
         sortTable(c)
     }
@@ -93,8 +90,11 @@ function sortTable(c) {
             one from current row and one from the next:*/
             x = rows[i].getElementsByTagName("TD")[c];
             y = rows[i + 1].getElementsByTagName("TD")[c];
+
+
+
             //check if the two rows should switch place:
-            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            if (parseFloat(x.innerHTML) > parseFloat(y.innerHTML)) {
                 //if so, mark as a switch and break the loop:
                 shouldSwitch = true;
                 break;
